b27bc261e6228db7290b597ca73a4933fdd3cca5
shingo <shingo.fukamatsu@gmail.com>
2024-09-22 22:14:08 +0900

    pipトライ

--- tools/hellow.py
+++ tools/hellow.py
@@ -1,2 +1,8 @@
+import requests
+
 print("pythonが実行されました")
 print("GithubActionsでpythonを動かすことに成功しました")
+
+# Webサイトの情報を変数rに格納し、そのうちのテキスト部分を出力
+r = requests.get("https://news.yahoo.co.jp/")
+print(r.text)
