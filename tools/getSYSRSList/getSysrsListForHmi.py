ccc674004b3c941477dad521db9094c47b044a8c
shingo <shingo.fukamatsu@gmail.com>
2024-11-10 18:15:26 +0900

    HMIリスト

--- tools/getSYSRSList/getSysrsListForHmi.py
+++ tools/getSYSRSList/getSysrsListForHmi.py
@@ -3,8 +3,10 @@ import csv
 import glob
 
 def export_requirement_id_list_to_csv():
+    # 初期化
+    output_data = []
     # 3.5	No3.1～3.4の処理を、すべての.adocファイルに対し実施する
-    adoc_files = glob.glob("*.adoc")
+    adoc_files = glob.glob("**/*.adoc", recursive=True) # 再帰的に.adocファイルを検索
     for file in adoc_files:
         # 3.1	.adocファイルを開く
         with open(file, "r", encoding="utf-8") as f:
@@ -26,11 +28,10 @@ def export_requirement_id_list_to_csv():
         for idx, line in enumerate(extracted_lines):
             if ":id:" in line:
                 upper_id = re.search(r"SYSRS\d+-\d+", line).group()
-            else:
+            elif "{id}" in line:
                 extracted_lines[idx] = line.replace("{id}", upper_id)
 
-        # 3.4	要求ID名に置換したリストから要求ID、HMI担当に依頼したいラベルを抽出し、CSVとして出力する
-        output_data = []
+        # 3.4	要求ID名に置換したリストから要求ID、HMI担当に依頼したいラベルを抽出する
         for line in extracted_lines:
             if ":id:" in line:
                 continue
@@ -49,14 +50,15 @@ def export_requirement_id_list_to_csv():
             # 出力用HMIラベルを抽出
             hmi_label_match = re.search(r"\[HMI:.*\]", line)
             hmi_label_tmp = hmi_label_match.group() if hmi_label_match else ""
-            hmi_label_tmp_matches = re.findall(r"\d+", hmi_label_tmp)
-            hmi_label = ",".join([f"disp{num}" for num in hmi_label_tmp_matches])
-            output_data.append([req_id, hmi_label])
+            hmi_label_tmp = re.sub(r"(\d+)", r"disp\1", hmi_label_tmp)            #数字の前にdispを付与
+            hmi_label = hmi_label_tmp.replace("[HMI:", "").replace("]", "")       #[HMI:　と ] の文字列を空欄に置換
+            #CSV出力用リストを更新
+            output_data.append([req_id, hmi_label, file])
 
-        csv_file = file.replace(".adoc", ".csv")
-        with open(csv_file, "w", newline="", encoding="utf-8") as f:
+        # 3.6	出力用要求ID、出力用HMIラベルをCSVに出力する
+        with open("output_req_id_list.csv", "w", newline="", encoding="utf-8") as f:
             writer = csv.writer(f)
-            writer.writerow(["要求ID", "HMIラベル"])
+            writer.writerow(["要求ID", "HMIラベル", "ファイル名"])
             writer.writerows(output_data)
 
 export_requirement_id_list_to_csv()
